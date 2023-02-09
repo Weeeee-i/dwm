83475d1d82eddad7534d8b1613815c1bba918464
Weeeee-i <alaw22022@gmail.com>
2023-02-09 18:43:27 +0800

    简单移植主题 https://github.com/catppuccin/catppuccin

--- config.def.h
+++ config.def.h
@@ -1,4 +1,5 @@
 /* See LICENSE file for copyright and license details. */
+#include <X11/XF86keysym.h>
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
@@ -11,17 +12,43 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;     /* 0 means no bar */
 static const int topbar             = 1;     /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
+static const char *fonts[]          = { "monospace:size=16" };
+static const char dmenufont[]       = "monospace:size=16";
+
+static const char col_gray1[]   = "#222222";
+static const char col_gray2[]   = "#444444";
+static const char col_gray3[]   = "#bbbbbb";
+static const char col_gray4[]   = "#eeeeee";
+static const char col_cyan[]    = "#005577";
+static const char rosewater[]	= "#f2d5cf";
+static const char flamingo[]	= "#eebebe";
+static const char pink[]		= "#f4b8e4";
+static const char mauve[]		= "#ca9ee6";
+static const char red[]			= "#e78284";
+static const char maroon[]		= "#ea999c";
+static const char peach[]		= "#ef9f76";
+static const char green[]		= "#a6d189";
+static const char teal[]		= "#81c8be";
+static const char sky[]			= "#99d1db";
+static const char sapphire[]	= "#85c1dc";
+static const char blue[]		= "#85c1dc";
+static const char lavender[]	= "#babbf1";
+static const char text[]		= "#c6d0f5";
+static const char subtext1[]	= "#b5bfe2";
+static const char subtext0[]	= "#a5adce";
+static const char overlay2[]	= "#949cbb";
+static const char overlay1[]	= "#838ba7";
+static const char overlay0[]	= "#737994";
+static const char surface2[]	= "#626880";
+static const char surface1[]	= "#51576d";
+static const char surface0[]	= "#414559";
+static const char base[]		= "#303446";
+static const char mantle[]		= "#292c3c";
+static const char crust[]		= "#232634";
+static const char *colors[][3]	= {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { text, base, mauve },
+	[SchemeSel]  = { text, base,  sky  },
 };
 
 /* tagging */
@@ -35,6 +62,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "KeePassXC",	NULL,		NULL,		0,				1,		-1 },
 };
 
 /* layout(s) */
@@ -51,7 +79,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -62,8 +90,15 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *dmenucmd[]	= { "rofi", "-show", "drun", NULL };
+static const char *termcmd[]	= { "alacritty", NULL };
+
+static const char *upvol[]	= { "amixer", "-q", "sset", "Master", "1%+", NULL };
+static const char *downvol[]	= { "amixer", "-q", "sset", "Master", "1%-", NULL };
+static const char *mute[]	= { "amixer", "-q", "set", "Master", "toggle", NULL };
+
+static const char *lightup[]	= { "light", "-A", "1", NULL };
+static const char *lightdown[]	= { "light", "-U", "1", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -104,6 +139,13 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+
+	{ 0,				XF86XK_AudioRaiseVolume,	spawn,	{.v = upvol }	},
+	{ 0,				XF86XK_AudioLowerVolume,	spawn,	{.v = downvol }	},
+	{ 0,				XF86XK_AudioMute,			spawn,	{.v = mute }	},
+	{ 0,				XF86XK_MonBrightnessUp,		spawn,	{.v = lightup }	},
+	{ 0,				XF86XK_MonBrightnessDown,	spawn,	{.v = lightdown}},
+
 };
 
 /* button definitions */
